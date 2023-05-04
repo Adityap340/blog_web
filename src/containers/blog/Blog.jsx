14e75ee3ed8d775c10878580231f900118a4b650
Adityap340 <aditya_patil_comp@moderncoe.edu.in>
2023-05-04 20:46:47 +0530

    First commit

---
+++ src/containers/blog/Blog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Article from '../../components/article/Article';
+import './blog.css';
+const Blog = () => {
+  return (
+    <div>Blog</div>
+  )
+}
+
+export default Blog;
